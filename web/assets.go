d34bebebd647aef632bec381f1085dae6a7e97d5
Zayne Chuah <zheng_hung@hotmail.com>
2026-09-06 23:06:24 +0800

    feat(web): add paddle icon favicon + header (#7) (#11)
    - embed favicon.png/ico, apple-touch-icon, icon-512
    - add link tags + header img in midnight-band

--- web/assets.go
+++ web/assets.go
@@ -2,5 +2,5 @@ package webassets
 
 import "embed"
 
-//go:embed index.html app.js style.css
+//go:embed index.html app.js style.css favicon.png favicon.ico apple-touch-icon.png icon-512.png favicon-16.png
 var FS embed.FS
